13d95c357759457fc0a10b110ea8bbde4376299e
TungCo89 <tungdao0101@gmail.com>
2025-09-17 16:40:07 +0700

    Update database.ts

--- API/src/database.ts
+++ API/src/database.ts
@@ -7,8 +7,8 @@ dotenv.config();
 const db: Pool = mysql.createPool({
   host: process.env.DB_HOST || 'localhost',
   user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || 'TTTMy2403',
-  database: process.env.DB_NAME || 'LuyenThi912',
+  password: process.env.DB_PASSWORD || 'root',
+  database: process.env.DB_NAME || 'tiemtrachanh1997',
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
